458e7806ae15a67a0523935dd35d1b4e1f68c53b
PrescilliaDSD <dossantosdias.prescillia@gmail.com>
2019-03-02 12:34:15 +0100

    function diceBlock to select every dice

--- js/dice-animation.js
+++ js/dice-animation.js
@@ -1,8 +1,8 @@
 var app = {
   init : function () {
-    var diceButton =document.querySelector('.throw-dice');
+    var diceButton = document.querySelector('.throw-dice');
     // quand on click sur le bouton "lancer les dés", on applique la fonction diceResult
-    diceButton.addEventListener('click', app.diceResult);
+    diceButton.addEventListener('click', app.diceResult);   
   },
 
   diceResult : function () {
@@ -16,20 +16,54 @@ var app = {
       '<i class="all-dice fas fa-dice-six"></i>',
     ];
 
-    var changingDice = document.querySelectorAll('.dice-button');
-
-    for (var index = 0; index < changingDice.length; index += 1) {
-      changingDice[index].innerHTML = dice[Math.floor(Math.random() * 6)];
+    var changedDice = document.querySelectorAll('.dice-button');
+    
+    for (var index = 0; index < changedDice.length; index += 1) {
+      if (changedDice[index].classList.contains('selected') === false) {
+      changedDice[index].innerHTML = dice[Math.floor(Math.random() * 6)];
     }
+    
+    changedDice[0].addEventListener('click', app.diceBlock1);
+    changedDice[1].addEventListener('click', app.diceBlock2);
+    changedDice[2].addEventListener('click', app.diceBlock3);
+    changedDice[3].addEventListener('click', app.diceBlock4);
+    changedDice[4].addEventListener('click', app.diceBlock5);
 
-    changingDice.addEventListener('click', app.blockDice);
+  }
   },
 
-  blockDice = function() {
-    
-  }
+  diceBlock1 : function() {
+      var dice1 = document.querySelector('#dice1');
+      dice1.classList.toggle('selected');
+      dice1.classList.toggle('text-info');      
+  },
 
+  diceBlock2 : function() {
+    var dice2 = document.querySelector('#dice2');
+    dice2.classList.toggle('selected');
+    dice2.classList.toggle('text-info');
+},
+
+  diceBlock3 : function() {
+  var dice3 = document.querySelector('#dice3');
+  dice3.classList.toggle('selected');
+  dice3.classList.toggle('text-info');
+},
+
+  diceBlock4 : function() {
+  var dice4 = document.querySelector('#dice4');
+  dice4.classList.toggle('selected');
+  dice4.classList.toggle('text-info');
+},
+
+  diceBlock5 : function() {
+  var dice5 = document.querySelector('#dice5');
+  dice5.classList.toggle('selected');
+  dice5.classList.toggle('text-info');
+},
 }
+
+
 
 
 // Lorsque la page a fini de charger, je veux lancer la fonction init
